8a9af7a53d36cfe2f07258a589aad35480715368
Antti Lylander <antti.lylander@gmail.com>
2018-10-05 10:01:59 +0300

    refactor List.js

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ListRow from './ListRow';
 import ListName from './ListName';
 import AddRow from './AddRow';
+import Table from './Table';
 
 export default class List extends Component {
     constructor (props) {
@@ -18,85 +19,71 @@ export default class List extends Component {
     }
 
     onRowSelectChange() {
-
+        //make it save the last selected row when selection changes
+        //maybe only when there has been a change
+        //how to check that? Set a state when editing?
     }
 
     render () {
         const items = this.props.list.items;
-
+        //this function is used to avoid repetition in listItems and reserveItems
+        const row = item => {
+            return (
+                <ListRow
+                    onRowSelectChange={this.onRowSelectChange}
+                    selectedRow={this.state.selectedRow}
+                    onRowSelect={this.onRowSelect}
+                    onItemDelete={this.props.onItemDelete}
+                    onItemEdit={this.props.onItemEdit}
+                    item={item}
+                    key={item._id}
+                />
+            );
+        }
+        //create an array of items that will be listed on the 'needed'
+        //part of the shoppinglist
         const listItems = items.map(item => {
 
             if (item.isNeeded) {
-                return (
-                    <ListRow
-                        onRowSelectChange={this.onRowSelectChange}
-                        selectedRow={this.state.selectedRow}
-                        onRowSelect={this.onRowSelect}
-                        onItemDelete={this.props.onItemDelete}
-                        onItemEdit={this.props.onItemEdit}
-                        item={item}
-                        key={item._id}
-                    />
-                );
+                return row(item);
             }
             return null;
         });
-
+        //create an array of reserved items
         const reserveItems = items.map(item => {
 
             if (!item.isNeeded) {
-                return (
-                    <ListRow
-                        onRowSelectChange={this.onRowSelectChange}
-                        selectedRow={this.state.selectedRow}
-                        onRowSelect={this.onRowSelect}
-                        onItemDelete={this.props.onItemDelete}
-                        onItemEdit={this.props.onItemEdit}
-                        item={item}
-                        key={item._id}
-                    />
-                );
+                return row(item);
             }
             return null;
 
         });
         return (
-
             <div>
                 <ListName
                     key={this.props.list._id}
                     onListEdit={this.props.onListEdit}
-                    listName={this.props.list.listName} />
-                <h3>Ostettavat</h3>
+                    listName={this.props.list.listName}
+                />
 
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Tuote</th>
-                            <th scope="col">Kategoria</th>
-                            <th scope="col">Ostettu</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <AddRow onItemAdd={this.props.onItemAdd}/>
-                        {listItems}
-                    </tbody>
-                </table>
+                <h3>Ostettavat</h3>
+                <Table
+                    listItems={listItems}
+                    onItemAdd={this.props.onItemAdd}
+                />
+
 
                 <h3>Tuotereservi</h3>
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Tuote</th>
-                            <th>Kategoria</th>
-                            <th>Ostettaviin</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {reserveItems}
-                    </tbody>
-                </table>
-                <button onClick={() => this.props.onListDelete(this.props.list._id)}>Poista lista</button>
+                <Table
+                    listItems={reserveItems}
+                    onItemAdd={this.props.onItemAdd}
+                />
+                <button
+                    onClick={() => {
+                        this.props.onListDelete(this.props.list._id)
+                    }}
+                >Poista lista
+                </button>
             </div>
 
         );

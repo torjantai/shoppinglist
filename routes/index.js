91a55adf56ff774c04bbc88f3157b60e6aad817d
Antti Lylander <antti.lylander@gmail.com>
2018-10-23 09:33:10 +0300

    add put and delete list

--- routes/index.js
+++ routes/index.js
@@ -14,31 +14,32 @@ router.use(passport.authenticate('jwt', { session : false }));
 //as defined in server.js app.use('/shoppinglist', routes)
 
 //router.param() will catch any route that includes 'username'
+//user document will be stored on req.userDocument
 router.param('username', function(req, res, next, username) {
         console.log('router.param', username);
         //check that req.user from token equals to username on route
         //req.user comes from passport.authenticate
-            if (req.user.userName !== username) {
+        if (req.user.userName !== username) {
             return res.send(401);
         }
         //find user and store the doc on req object
         User.findOne( { userName: username }, function (err, doc) {
 
-                if (err) return next(err);
-                if (!doc) {
-                    err = new Error(`No user found with id ${username}`);
-                    err.status = 404;
-                    return next(err);
-                }
-                // console.log(doc);
-                req.userDocument = doc;
-                return next();
+            if (err) return next(err);
+            if (!doc) {
+                err = new Error(`No user found with id ${username}`);
+                err.status = 404;
+                return next(err);
+            }
+            // console.log(doc);
+            req.userDocument = doc;
+            return next();
             });
 });
 
 
 //GET a single user with given id
-router.get('/:username', function(req, res, next) {
+router.get('/:username', function(req, res) {
         console.log('get username');
         res.json(req.userDocument);
 });
@@ -66,168 +67,41 @@ router.get('/:username/:listId', function(req, res, next) {
         err.status = 404;
         return next(err);
     }
-
     res.json(list);
 });
 
+router.put('/:username/:listId', function(req, res, next) {
+    console.log('route /:username/:listId', 'body;', req.body )
+    let list = req.userDocument.lists.id(req.params.listId);
+    if ( !list ) {
+        const err = new Error(`No list found with id ${req.params.listId}`);
+        err.status = 404;
+        return next(err);
+    }
 
-//NOTE: everything below is based on the old data structure
-//
-// //POST lists -- create a list
-// router.post('/', function(req, res, next) {
-//     console.log(req.body);
-//     const list = new List(req.body);
-//     list.save(function(err, list) {
-//         if (err) return next(err);
-//         res.status(201);
-//         res.json(list);
-//     });
-// });
-//
-// //GET a single list with given id
-// router.get('/:listId', function (req, res, next) {
-//     List.findById(req.params.listId, function (err, doc) {
-//
-//         if (err) return next(err);
-//         if (!doc) {
-//             err = new Error(`No list found with id ${req.params.listId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         res.json(doc);
-//     });
-// });
-//
-// //delete a list
-// router.delete('/:listId/', function(req, res, next) {
-//     List.findById(req.params.listId, function (err, doc) {
-//         if (err) return next(err);
-//         if (!doc) {
-//             err = new Error(`No list found with id ${req.params.listId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//
-//         doc.remove(function (err) {
-//             if (err) return next(err);
-//             //after deleting, respond with all lists
-//             List.find({})
-//                     .exec(function(err, items) {
-//                         if (err) return next(err);
-//                         res.status(201);
-//                         res.json(items);
-//                     });
-//         });
-//
-//     });
-// });
-//
-// //edit a list
-// router.put('/:listId/', function(req, res, next) {
-//     List.findById(req.params.listId, function (err, doc) {
-//         if (err) return next(err);
-//         if (!doc) {
-//             err = new Error(`No list found with id ${req.params.listId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         doc.listName = req.body.listName;
-//         doc.save(function(err) {
-//             if (err) return next(err);
-//             res.status(201);
-//             res.json(doc);
-//         });
-//     });
-// });
-//
-// //POST /:id/item
-// //create an item in a given list
-// router.post('/:listId/items', function(req, res, next) {
-//     console.log(req.body);
-//     List.findById(req.params.listId, function (err, doc) {
-//         //console.dir(doc);
-//         if (err) return next(err);
-//         if (!doc) {
-//             err = new Error(`No list found with id ${req.params.listId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         if (!req.body.article || !req.body.category) {
-//             err = new Error(`article and category mandatory`);
-//             console.log(err);
-//             err.status = 500;
-//             return next(err);
-//         }
-//         doc.items.push(req.body);
-//         doc.save(function(err, question) {
-//             if (err) return next(err);
-//             res.status(201);
-//             res.json(doc);
-//         });
-//     });
-// });
-//
-//
-//
-//
-// //edit a specific item in a given list
-// //update method is defined on ItemSchema
-// router.put('/:listId/items/:itemId', function(req, res, next) {
-//     List.findById(req.params.listId, function (err, doc) {
-//         if (err) return next(err);
-//         if (!doc) {
-//             err = new Error(`No list found with id ${req.params.listId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         const item = doc.items.id(req.params.itemId);
-//         if (!item) {
-//             err = new Error(`No item found with id ${req.params.itemId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         item.update(req.body, function (err, result) {
-//             if (err) return next(err);
-//             res.json(doc);
-//         });
-//     });
-// });
-//
-//
-// //delete a specific item in a given lists
-// router.delete('/:listId/items/:itemId', function(req, res, next) {
-//     List.findById(req.params.listId, function (err, doc) {
-//         if (err) return next(err);
-//         if (!doc) {
-//             err = new Error(`No list found with id ${req.params.listId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         const item = doc.items.id(req.params.itemId);
-//         if (!item) {
-//             err = new Error(`No item found with id ${req.params.itemId}`);
-//             err.status = 404;
-//             return next(err);
-//         }
-//         item.remove(function (err) {
-//             if (err) return next(err);
-//             doc.save(function(err, question) {
-//                 if (err) return next(err);
-//                 res.status(201);
-//                 res.json(doc);
-//             });
-//
-//         });
-//     });
-// });
+    list.set(req.body);
+    req.userDocument.save(function(err, doc) {
+        if (err) return next(err);
+        res.status(201);
+        res.json(req.userDocument);
+    });
+});
 
+router.delete('/:username/:listId', function(req, res, next) {
+    let list = req.userDocument.lists.id(req.params.listId);
+    if ( !list ) {
+        const err = new Error(`No list found with id ${req.params.listId}`);
+        err.status = 404;
+        return next(err);
+    }
 
-
-
-
-
-
-
+    list.remove();
+    req.userDocument.save(function(err, doc) {
+        if (err) return next(err);
+        res.status(201);
+        res.json(req.userDocument);
+    });
+});
 
 
 module.exports = router;
